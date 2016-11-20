88c63a76f0eeb72bf44b819b1725057cb3911dd9
Martin Pieuchot <mpi@openbsd.org>
2016-11-20 14:38:12 +0100

    Try to be more bug-compatible with readelf(1) --debug-dump option.
    While here split attribute and form parsing in dump_cu() to make it
    easier to reuse the code.

--- readdwarf.c
+++ readdwarf.c
@@ -34,6 +34,10 @@
 
 #include "dw.h"
 
+#ifndef nitems
+#define nitems(_a)	(sizeof((_a)) / sizeof((_a)[0]))
+#endif
+
 #define DEBUG_ABBREV	".debug_abbrev"
 #define DEBUG_INFO	".debug_info"
 #define DEBUG_LINE	".debug_line"
@@ -57,6 +61,12 @@ int		 elf_getsymtab(const char *, const char *, size_t,
 		     const Elf_Sym **, size_t *);
 int		 elf_getsection(const char *, const char *, const char *,
 		     size_t, const char **, size_t *);
+
+uint64_t	 dav2val(struct dwaval *, size_t);
+const char	*dav2str(struct dwaval *);
+const char	*enc2name(unsigned short);
+const char	*lang2name(unsigned short);
+const char	*inline2name(unsigned short);
 
 __dead void
 usage(void)
@@ -184,18 +194,20 @@ dwarf_dump(const char *p, size_t filesize, uint8_t flags)
 
 		SIMPLEQ_INIT(&dabq);
 
+		printf("Contents of the %s section:\n\n", DEBUG_ABBREV);
 		while (dw_ab_parse(&abbrev, &dabq) == 0) {
 			struct dwabbrev *dab;
 
+ 			printf("  Number TAG\n");
 			SIMPLEQ_FOREACH(dab, &dabq, dab_next) {
 				struct dwattr *dat;
 
-				printf("[%llu] %s\t\t[%s children]\n",
+				printf("   %llu      %s    [%s children]\n",
 				    dab->dab_code, dw_tag2name(dab->dab_tag),
 				    (dab->dab_children) ? "has" : "no");
 
 				SIMPLEQ_FOREACH(dat, &dab->dab_attrs, dat_next){
-					printf("      %s\t%s\n",
+					printf("    %-18s %s\n",
 					    dw_at2name(dat->dat_attr),
 					    dw_form2name(dat->dat_form));
 				}
@@ -239,118 +251,209 @@ dump_cu(struct dwcu *dcu)
 		    dw_tag2name(die->die_dab->dab_tag));
 
 		SIMPLEQ_FOREACH(dav, &die->die_avals, dav_next) {
+			uint64_t attr = dav->dav_dat->dat_attr;
 			uint64_t form = dav->dav_dat->dat_form;
+			uint64_t val = 0;
+			const char *str = NULL;
 
-			printf("     %-18s: ",
-			    dw_at2name(dav->dav_dat->dat_attr));
+			printf("     %-18s: ", dw_at2name(attr));
 
-			switch (form) {
-			case DW_FORM_addr:
-			case DW_FORM_ref_addr:
-				if (dcu->dcu_psize == sizeof(uint32_t))
-					printf("0x%x\n", dav->dav_u32);
-				else
-					printf("0x%llx\n", dav->dav_u64);
+			val = dav2val(dav, dcu->dcu_psize);
+			str = dav2str(dav);
+			if (val == (uint64_t)-1 && str == NULL) {
+				printf("%s: %llu\n", dw_form2name(form), form);
+				continue;
+			}
+
+			switch (attr) {
+			case DW_AT_producer:
+			case DW_AT_name:
+			case DW_AT_comp_dir:
+				switch (form) {
+				case DW_FORM_string:
+					printf("%s", str);
+					break;
+				case DW_FORM_strp:
+					printf("(indirect string, offset:"
+					    " 0x%llx): %s", val, str);
+					break;
+				default:
+					printf(" %s", dw_form2name(form));
+					break;
+				}
 				break;
-			case DW_FORM_block1:
-			case DW_FORM_block2:
-			case DW_FORM_block4:
-			case DW_FORM_block:
-				printf("%zu byte block\n", dav->dav_buf.len);
+			case DW_AT_byte_size:
+			case DW_AT_decl_file:
+			case DW_AT_decl_line:
+			case DW_AT_upper_bound:
+			case DW_AT_prototyped:
+			case DW_AT_external:
+			case DW_AT_declaration:
+			case DW_AT_call_file:
+			case DW_AT_call_line:
+				printf("%llu", val);
 				break;
-			case DW_FORM_flag:
-			case DW_FORM_data1:
-				printf("%u\n", dav->dav_u8);
+			case DW_AT_inline:
+				printf("%llu\t(%s)", val, inline2name(val));
 				break;
-			case DW_FORM_data2:
-				printf("%u\n", dav->dav_u16);
+			case DW_AT_stmt_list:
+			case DW_AT_low_pc:
+			case DW_AT_high_pc:
+			case DW_AT_ranges:
+				printf("0x%llx", val);
 				break;
-			case DW_FORM_data4:
-				printf("%u\n", dav->dav_u32);
+			case DW_AT_language:
+				printf("%llu\t(%s)", val, lang2name(val));
 				break;
-			case DW_FORM_data8:
-				printf("%llu\n", dav->dav_u64);
+			case DW_AT_encoding:
+				printf("%llu\t(%s)", val, enc2name(val));
 				break;
-			case DW_FORM_ref1:
-				printf("<%x>\n", dav->dav_u8);
+			case DW_AT_location:
+			case DW_AT_frame_base:
+			case DW_AT_data_member_location:
+				switch (form) {
+				case DW_FORM_block1:
+				case DW_FORM_block2:
+				case DW_FORM_block4:
+				case DW_FORM_block:
+					printf("%llu byte block", val);
+					break;
+				case DW_FORM_data1:
+				case DW_FORM_data2:
+				case DW_FORM_data4:
+				case DW_FORM_data8:
+					printf("0x%llx\t(location list)", val);
+					break;
+				default:
+					printf("%s", dw_form2name(form));
+					break;
+				}
 				break;
-			case DW_FORM_ref2:
-				printf("<%x>\n", dav->dav_u16);
+			case DW_AT_type:
+			case DW_AT_sibling:
+			case DW_AT_abstract_origin:
+				printf("<%llx>", val);
 				break;
-			case DW_FORM_ref4:
-				printf("<%x>\n", dav->dav_u32);
-				break;
-			case DW_FORM_ref8:
-				printf("<%llx>\n", dav->dav_u64);
-				break;
-			case DW_FORM_string:
-				printf("%s\n", dav->dav_str);
-				break;
-			case DW_FORM_strp:
-				printf("(indirect string, offset: 0x%x): %s\n",
-				    dav->dav_u32, dstrbuf + dav->dav_u32);
-				break;
-			case DW_FORM_flag_present:
-				printf("1\n");
 			default:
-				printf("%s\n", dw_form2name(form));
+				printf("unimplemented");
 				break;
 			}
+			printf("\n");
 		}
 	}
 
 	return 0;
 }
 
-#if 0
-void
-dump_type(struct dwcu_queue *dcuq)
+uint64_t
+dav2val(struct dwaval *dav, size_t psize)
 {
-	struct dwcu *dcu;
-	struct dwdie *die;
-	struct dwaval *dav;
+	uint64_t val = (uint64_t)-1;
 
-	SIMPLEQ_FOREACH(dcu, dcuq, dcu_next) {
-		int i = 0;
+	switch (dav->dav_dat->dat_form) {
+	case DW_FORM_addr:
+	case DW_FORM_ref_addr:
+		if (psize == sizeof(uint32_t))
+			val = dav->dav_u32;
+		else
+			val = dav->dav_u64;
+		break;
+	case DW_FORM_block1:
+	case DW_FORM_block2:
+	case DW_FORM_block4:
+	case DW_FORM_block:
+		val = dav->dav_buf.len;
+		break;
+	case DW_FORM_flag:
+	case DW_FORM_data1:
+	case DW_FORM_ref1:
+		val = dav->dav_u8;
+		break;
+	case DW_FORM_data2:
+	case DW_FORM_ref2:
+		val = dav->dav_u16;
+		break;
+	case DW_FORM_data4:
+	case DW_FORM_ref4:
+		val = dav->dav_u32;
+		break;
+	case DW_FORM_data8:
+	case DW_FORM_ref8:
+		val = dav->dav_u64;
+		break;
+	case DW_FORM_strp:
+		val = dav->dav_u32;
+		break;
+	case DW_FORM_flag_present:
+		val = 1;
+		break;
+	default:
+		break;
+	}
 
-		SIMPLEQ_FOREACH(die, &dcu->dcu_dies, die_next) {
-			const char *name = NULL;
-			uint8_t bits = 0, encoding = 0;
-			size_t off = 0, ref = 0;
+	return val;
+}
 
-			if (die->die_dab->dab_tag != DW_TAG_base_type &&
-			    die->die_dab->dab_tag != DW_TAG_typedef)
-			    	continue;
+const char *
+dav2str(struct dwaval *dav)
+{
+	const char *str = NULL;
 
-			SIMPLEQ_FOREACH(dav, &die->die_avals, dav_next) {
-				switch(dav->dav_dat->dat_attr) {
-				case DW_AT_byte_size:
-					bits = dav->dav_u8;
-					break;
-				case DW_AT_encoding:
-					encoding = dav->dav_u8;
-					break;
-				case DW_AT_type:
-					ref = dav->dav_u32;
-					break;
-				case DW_AT_name:
-					off = dav->dav_u32;
-					name = dstr.buf + dav->dav_u32;
-					break;
-				default:
-					break;
-				}
-			}
+	switch (dav->dav_dat->dat_form) {
+	case DW_FORM_string:
+		str = dav->dav_str;
+		break;
+	case DW_FORM_strp:
+		str = dstrbuf + dav->dav_u32;
+		break;
+	default:
+		break;
+	}
 
-			if (die->die_dab->dab_tag == DW_TAG_base_type) {
-				printf("  <%d> %s encoding=0x%x offset=%zu "
-				    "bits=%u\n", ++i, name, encoding,
-				    off, bits);
-			} else {
-				printf("  <%d> typedef %s refers to %zu\n",
-				    ++i, name, ref);
-			}
-		}
+	return str;
+}
+
+const char *
+enc2name(unsigned short enc)
+{
+	static const char *enc_name[] = { "address", "boolean", "complex float",
+	    "float", "signed", "signed char", "unsigned", "unsigned char",
+	    "imaginary float", "packed decimal", "numeric string", "edited",
+	    "signed fixed", "unsigned fixed", "decimal float" };
+
+	if (enc > 0 && enc <= nitems(enc_name))
+		return enc_name[enc - 1];
+
+	return "invalid";
+}
+
+const char *
+lang2name(unsigned short lang)
+{
+	static const char *lang_name[] = { "ANSI C", "C", "Ada83", "C++",
+	    "Cobol74", "Cobol85", "Fortran77", "Fortran90", "Pascal83",
+	    "Modula2", "Java", "C99", "Ada95", "Fortran95", "PLI", "ObjC",
+	    "ObjC++", "UPC", "D" };
+
+	if (lang > 0 && lang <= nitems(lang_name))
+		return lang_name[lang - 1];
+
+	return "invalid";
+}
+
+const char *
+inline2name(unsigned short inl)
+{
+	switch (inl) {
+	case DW_INL_not_inlined:
+		return "not inlined";
+	case DW_INL_inlined:
+		return "inlined";
+	case DW_INL_declared_not_inlined:
+		return "declared as inlined and not inlined";
+	case DW_INL_declared_inlined:
+		return "declared as inline and inlined";
+	default:
+		return "invalid";
 	}
 }
-#endif
