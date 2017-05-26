f907d9f23fa0300540670835616446d92ba21b1c
Martin Pieuchot <mpi@openbsd.org>
2017-05-26 10:07:15 +0200

    Unserstand DW_OP_bregN locations.

--- dw.c
+++ dw.c
@@ -629,8 +629,17 @@ dw_loc_parse(struct dwbuf *dwbuf, uint8_t *pop, uint64_t *poper1,
 		*pop = op;
 
 	switch (op) {
+	case DW_OP_constu:
 	case DW_OP_plus_uconst:
+	case DW_OP_regx:
+	case DW_OP_piece:
 		dw_read_uleb128(dwbuf, &oper1);
+		break;
+
+	case DW_OP_consts:
+	case DW_OP_breg0 ... DW_OP_breg31:
+	case DW_OP_fbreg:
+		dw_read_sleb128(dwbuf, &oper1);
 		break;
 	default:
 		return ENOTSUP;
